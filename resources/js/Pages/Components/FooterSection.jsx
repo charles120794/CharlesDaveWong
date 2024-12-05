e0b20994d85bc618628b0304cbf2f2a988bf59be
Charles Wong <wongcharlesdave@gmail.com>
2024-12-05 23:29:06 +0800

    initial commit

---
+++ resources/js/Pages/Components/FooterSection.jsx
@@ -0,0 +1,168 @@
+import { Link } from "@inertiajs/react";
+import { CiLocationOn } from "react-icons/ci";
+import { CiHeadphones } from "react-icons/ci";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaSquareFacebook } from "react-icons/fa6";
+import { CiLinkedin } from "react-icons/ci";
+
+export default function FooterSection() {
+    const handleSubmit = () => {};
+    return (
+        <footer className="py-16 text-center text-sm text-black dark:text-white/70">
+            <div className="container mx-auto py-6">
+                <div className="grid grid-cols-1 items-start sm:grid-cols-2 md:grid-cols-3">
+                    {/* Column 1: Company Info */}
+                    <div className="space-y-5">
+                        <Link className="" href="/">
+                            <img
+                                src="http://probuilder-web-expert.test/Screenshot 2024-12-05 190108.png"
+                                className="h-24 -ml-6"
+                            />
+                        </Link>
+                        <div className="space-y-4 cursor-default">
+                            <div className="flex items-start space-x-5">
+                                <CiLocationOn size={30} strokeWidth={1} />
+                                <p className="text-lg">
+                                    Quezon City, Metro Manila
+                                </p>
+                            </div>
+                            <div className="flex items-start space-x-5 ml-[0.2px]">
+                                <CiHeadphones size={30} strokeWidth={1} />
+                                <p className="text-lg">0999 (955) (4869)</p>
+                            </div>
+                            <div className="flex items-start space-x-5 ml-[0.2px]">
+                                <MdOutlineEmail size={30} />
+                                <p>probuilderwebexph@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Column 2: About Us */}
+                    <div className="text-left">
+                        <div className="my-5">
+                            <h3 className="text-left text-white text-xl font-bold mb-4">
+                                Quick Links
+                            </h3>
+                        </div>
+                        <ul className="space-y-3 text-base font-medium">
+                            <li className="mb-2">
+                                <Link
+                                    href="#profile"
+                                    className=" hover:font-bold hover:text-white transition ease-in-out"
+                                >
+                                    Profile
+                                </Link>
+                            </li>
+                            <li className="mb-2">
+                                <Link
+                                    href="#about"
+                                    className=" hover:font-bold hover:text-white transition ease-in-out"
+                                >
+                                    About Us
+                                </Link>
+                            </li>
+                            <li className="mb-2">
+                                <Link
+                                    href="#contact"
+                                    className=" hover:font-bold hover:text-white transition ease-in-out"
+                                >
+                                    Contact Us
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                    {/* Column 3: Services */}
+                    {/* <div>
+                        <h3 className="text-lg font-bold mb-4">Follow Us</h3>
+                        <ul>
+                            <li className="mb-2">
+                                <a
+                                    href="/life-insurance"
+                                    className="hover:text-blue-400"
+                                >
+                                    Facebook
+                                </a>
+                            </li>
+                            <li className="mb-2">
+                                <a
+                                    href="/health-insurance"
+                                    className="hover:text-blue-400"
+                                >
+                                    Twitter
+                                </a>
+                            </li>
+                            <li className="mb-2">
+                                <a
+                                    href="/accidental-coverage"
+                                    className="hover:text-blue-400"
+                                >
+                                    Instagram
+                                </a>
+                            </li>
+                            <li className="mb-2">
+                                <a
+                                    href="/investment-plans"
+                                    className="hover:text-blue-400"
+                                >
+                                    Youtube
+                                </a>
+                            </li>
+                        </ul>
+                    </div> */}
+
+                    {/* Column 4: Connect with Us */}
+                    <div className="text-left">
+                        <h2 className=" text-xl text-white font-bold my-5">
+                            Subscribe to Our Newsletter
+                        </h2>
+                        <p className="text-sm mb-2">
+                            Stay updated with the latest offers and exclusive
+                            deals. Join our community today!
+                        </p>
+                        <form onSubmit={handleSubmit} className="mb-4">
+                            <input
+                                type="text"
+                                placeholder="Enter your email"
+                                className="bg-transparent border-none text-gray-200 mb-2 px-4 py-2 w-full focus:outline-none focus:ring focus:ring-gray-700 rounded-md"
+                            />
+                            <div className="border-b w-full mb-2"></div>
+                            <button className="w-full py-2 bg-black text-white border border-white px-4 rounded-lg hover:bg-gray-950 focus:ring focus:ring-red-300 transition ease-in-out">
+                                Subscribe
+                            </button>
+                        </form>
+                        <div className="hidden space-x-4">
+                            <a
+                                href="https://facebook.com"
+                                className="text-blue-500 hover:text-blue-600"
+                            >
+                                Facebook
+                            </a>
+                            <a
+                                href="https://twitter.com"
+                                className="text-blue-400 hover:text-blue-500"
+                            >
+                                Twitter
+                            </a>
+                            <a
+                                href="https://linkedin.com"
+                                className="text-blue-600 hover:text-blue-700"
+                            >
+                                LinkedIn
+                            </a>
+                            <a
+                                href="https://instagram.com"
+                                className="text-pink-500 hover:text-pink-600"
+                            >
+                                Instagram
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="text-center text-cyan-700 text-sm mt-8 border-t border-cyan-700 pt-4">
+                <p>&copy; 2024 Probuilder Web Expert. All Rights Reserved.</p>
+            </div>
+        </footer>
+    );
+}
---
+++ resources/js/Pages/Components/IntroSection.jsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+
+export default function IntroSection() {
+    const [animate, setAnimate] = useState({
+        visibleIntro0: false,
+        visibleIntro1: false,
+        visibleIntro2: false,
+        visibleIntro3: false,
+        visibleIntro4: false,
+        visibleIntro5: false,
+    });
+
+    useEffect(() => {
+        setTimeout(
+            () =>
+                setAnimate((prev) => ({
+                    ...prev,
+                    visibleIntro0: true,
+                })),
+            0
+        );
+        setTimeout(
+            () =>
+                setAnimate((prev) => ({
+                    ...prev,
+                    visibleIntro1: true,
+                })),
+            300
+        );
+        setTimeout(
+            () =>
+                setAnimate((prev) => ({
+                    ...prev,
+                    visibleIntro2: true,
+                })),
+            500
+        );
+        setTimeout(
+            () =>
+                setAnimate((prev) => ({
+                    ...prev,
+                    visibleIntro3: true,
+                })),
+            1000
+        );
+        setTimeout(
+            () =>
+                setAnimate((prev) => ({
+                    ...prev,
+                    visibleIntro4: true,
+                })),
+            1500
+        );
+        setTimeout(
+            () =>
+                setAnimate((prev) => ({
+                    ...prev,
+                    visibleIntro5: true,
+                })),
+            2000
+        );
+    }, []);
+
+    return (
+        <div className={`my-16 px-20 space-y-3 cursor-default`}>
+            <h2
+                className={`text-7xl text-white font-extrabold hover:text-shadow-lg transition-opacity ${
+                    animate.visibleIntro1 ? "opacity-100" : "opacity-0"
+                } duration-[1500ms]`}
+            >
+                Transforming{" "}
+            </h2>
+            <p
+                className={`text-6xl mx-3 font-extrabold text-blue-500 transition-opacity ${
+                    animate.visibleIntro2 ? "opacity-100" : "opacity-0"
+                } duration-[1500ms]`}
+            >
+                Ideas
+            </p>
+            <p
+                className={`text-5xl mx-3.5  font-extrabold text-sky-600 transition-opacity ${
+                    animate.visibleIntro3 ? "opacity-100" : "opacity-0"
+                } duration-[1500ms]`}
+            >
+                Into
+            </p>
+            <h2
+                className={`text-6xl mx-3 text-gray-200 font-extrabold transition-opacity ${
+                    animate.visibleIntro4 ? "opacity-100" : "opacity-0"
+                } duration-[1500ms]`}
+            >
+                Digital Reality
+            </h2>
+            <p
+                className={` text-base italic mx-3 transition left-6 ${
+                    animate.visibleIntro5
+                        ? "opacity-100 translate-x-20"
+                        : "opacity-0 translate-x-0"
+                } duration-[1500ms]`}
+            >
+                {"<-- Code your vision Online -->"}
+            </p>
+            <button className="py-3 px-10 border border-blue-600 bg-gray-900 text-base font-semibold text-blue-500 transition duration-300 rounded-md mt-10 mx-3 hover:shadow-md shadow-inherit hover:shadow-gray-500 hover:text-gray-100 active:bg-blue-700 hover:bg-blue-600">
+                LEARN MORE
+            </button>
+        </div>
+    );
+}
