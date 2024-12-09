e210962c5316d17352ad62ede4a07a9cc5de87c9
Charles Wong <wongcharlesdave@gmail.com>
2024-12-09 17:04:20 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+import inlineSource from "vite-plugin-inline-source";
 
 export default defineConfig({
+    define: {
+        "process.env": {
+            VITE_PUBLIC_PATH: "", // This makes asset URLs relative
+        },
+    },
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: "resources/js/app.jsx",
             refresh: true,
         }),
         react(),
+        inlineSource(),
     ],
 });
